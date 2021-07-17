e9b945106182aba9747df377c94a916bc34819d1
Luis Gonzalez <luis.a.gonzalez@live.com>
2021-07-17 10:59:02 -0700

    Added semi-colons

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const cool = require('cool-ascii-faces');
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+const express = require('express');
+const path = require('path');
+const PORT = process.env.PORT || 5000;
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
@@ -9,4 +9,4 @@ express()
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
   .get('/cool', (req, res) => res.send(cool()))
-  .listen(PORT, () => console.log(`Listening on ${PORT}`))
+  .listen(PORT, () => console.log(`Listening on ${PORT}`));
